eec42acc52094aa4683e506c5bdd5366401228e2
Ben2oid <joobence94@gmail.com>
2024-05-29 10:01:33 +0200

    feat: changed card to modal, and created another card for main page #3

--- frontend/src/Components/MovieCard/MovieCard.jsx
+++ frontend/src/Components/MovieCard/MovieCard.jsx
@@ -1,20 +1,28 @@
 import "./style.scss";
+const baseImageUrl = "https://image.tmdb.org/t/p";
 
 export const MovieCard = ({ detailedMovieData }) => {
-  const baseImageUrl = "https://image.tmdb.org/t/p";
+  const watchListBtnHandler = () => {
+    console.log("segg");
+  };
+
   return (
     <div className="movie-card">
-      <h2>{detailedMovieData.original_title}</h2>
-      <div className="movie-details">
-        <div className="movie-image-container">
-          <img
-            src={`${baseImageUrl}/w500/${detailedMovieData.poster_path}`}
-            alt="poster"
-          />
-        </div>
-        <div className="description">
-          <p>{detailedMovieData.overview}</p>
-        </div>
+      <div className="poster">
+        <button className="ribbon-btn" onClick={watchListBtnHandler}>
+          +
+        </button>
+        <img
+          className="poster-img"
+          src={`${baseImageUrl}/w500/${detailedMovieData.poster_path}`}
+          alt="poster"
+        />
+      </div>
+      <div className="score">
+        <p>100% SO BAD IT'S GOOD</p>
+      </div>
+      <div className="title">
+        <h2>{detailedMovieData.original_title}</h2>
       </div>
     </div>
   );
