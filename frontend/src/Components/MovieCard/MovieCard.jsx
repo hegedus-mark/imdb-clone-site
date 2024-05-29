1f8be20cc06cc91004f274197f47f2270d294217
Ben2oid <joobence94@gmail.com>
2024-05-29 09:07:33 +0200

    feat: basic card was built

--- frontend/src/Components/MovieCard/MovieCard.jsx
+++ frontend/src/Components/MovieCard/MovieCard.jsx
@@ -1,3 +1,21 @@
-export const MovieCard = () => {
-  return <div>MovieCard</div>;
+import "./style.scss";
+
+export const MovieCard = ({ detailedMovieData }) => {
+  const baseImageUrl = "https://image.tmdb.org/t/p";
+  return (
+    <div className="movie-card">
+      <h2>{detailedMovieData.original_title}</h2>
+      <div className="movie-details">
+        <div className="movie-image-container">
+          <img
+            src={`${baseImageUrl}/w500/${detailedMovieData.poster_path}`}
+            alt="poster"
+          />
+        </div>
+        <div className="description">
+          <p>{detailedMovieData.overview}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ frontend/src/Components/index.js
@@ -0,0 +1,3 @@
+export * from "./MovieCard/MovieCard";
+export * from "./Navbar/Navbar";
+export * from "./Searchbar/Searchbar";
