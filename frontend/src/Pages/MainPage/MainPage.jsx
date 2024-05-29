a9f9fc754dd51cce3da3eaa1c393b797b90793ba
Ben2oid <joobence94@gmail.com>
2024-05-29 11:35:45 +0200

    feat: added now play to main page #12

--- frontend/src/Pages/MainPage/MainPage.jsx
+++ frontend/src/Pages/MainPage/MainPage.jsx
@@ -7,19 +7,33 @@ export const MainPage = () => {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   };
-  const { data, error, loading, fetchData } = useFetchData(
+  const { data, error, loading } = useFetchData(
     true,
     "/api/movies/popular",
     options
   );
+  const {
+    data: playData,
+    error: playError,
+    loading: playLoading,
+  } = useFetchData(true, "/api/movies/nowplaying", options);
 
-  return loading ? (
+  return loading && playLoading ? (
     <h1>Please wait Loading</h1>
-  ) : error ? (
-    <h1>{error}</h1>
+  ) : error && playError ? (
+    <h1>{playError ? playError : error}</h1>
   ) : (
-    <div className="popular-movies">
-      <Carousel items={data.results} category={"Popular"} />
-    </div>
+    data &&
+    playData && (
+      <>
+        {console.log("yaaaaaaay", data, playData)}
+        <div className="popular-movies">
+          <Carousel items={data.results} category={"Popular"} />
+        </div>
+        <div className="nowPlaying-movies">
+          <Carousel items={playData.results} category={"Now Playing"} />
+        </div>
+      </>
+    )
   );
 };
