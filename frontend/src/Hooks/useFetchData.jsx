28b8caaf2c5fd600129fa250ad295349d664c8fb
Ben2oid <joobence94@gmail.com>
2024-05-29 10:56:20 +0200

    feat: fetchData hook created

---
+++ frontend/src/Hooks/useFetchData.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const useFetchData = (runOnLoad, url, options) => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchData = (url, options) => {
+    setLoading(true);
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log("recieved:", jsonData);
+        setData(jsonData);
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
+  };
+
+  useEffect(() => {
+    if (runOnLoad) {
+      fetchData(url, options);
+    }
+  }, []);
+
+  return { data, loading, error, fetchData };
+};
