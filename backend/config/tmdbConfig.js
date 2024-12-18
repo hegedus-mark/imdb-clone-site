455438da4c7a87ff68540ba6ce1c070939e0da95
hegedus-mark <hegedusmark2018@gmail.com>
2024-12-18 15:47:27 +0100

    refactor: Rewrite genreController, Add tmdb Urls to tmdbConfig

--- backend/config/tmdbConfig.js
+++ backend/config/tmdbConfig.js
@@ -4,7 +4,24 @@ export const TMDB_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + ACCESS_TOKEN,
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
   },
 };
-export const BASE_URL = "https://api.themoviedb.org/3";
+
+export const TMDB_URLS = {
+  BASE: "https://api.themoviedb.org/3",
+  DISCOVER: "/discover/movie",
+  GENRES: "/genre/movie/list",
+  MOVIE: "/movie",
+  SEARCH: "/search/movie",
+};
+
+export const buildTMDBUrl = (path, queryParams = {}) => {
+  const url = new URL(`${TMDB_URLS.BASE}${path}`);
+  Object.entries(queryParams).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      url.searchParams.append(key, value);
+    }
+  });
+  return url.toString();
+};
--- backend/controllers/genreController.js
+++ backend/controllers/genreController.js
@@ -1,36 +1,39 @@
-import { TMDB_OPTIONS } from "../config/tmdbConfig.js";
+import { TMDB_OPTIONS, TMDB_URLS, buildTMDBUrl } from "../config/tmdbConfig.js";
 import { saveMoviesToDB } from "../utils/saveMovies.js";
+import { asyncHandler } from '../middleware/asyncHandler.js';
+import { AppError } from '../errors/AppError.js';
 
+export const getMoviesByGenre = asyncHandler(async (req, res) => {
+  const genreId = req.params.genre;
+  const page = req.query.page || 1;
 
-export const getMoviesByGenre = async (req, res) => {
-  const ID = req.params.genre;
-  const { page } = req.query;
-  const reqPage = page ? page : 1;
-  try {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${reqPage}&sort_by=popularity.desc&with_genres=${ID}`,
-      TMDB_OPTIONS
-    )
-      .then((res) => res.json())
-      .then(async (json) => {
-        await saveMoviesToDB(json.results);
-        res.json(json);
-      })
-      .catch((err) => console.error(err));
-  } catch (error) {
-    res.status(506).json({ message: "something wrong" });
+  const url = buildTMDBUrl(TMDB_URLS.DISCOVER, {
+    include_adult: false,
+    include_video: false,
+    language: 'en-US',
+    page,
+    sort_by: 'popularity.desc',
+    with_genres: genreId
+  });
+
+  const response = await fetch(url, TMDB_OPTIONS);
+  if (!response.ok) {
+    throw new AppError('Failed to fetch movies by genre', response.status);
   }
-};
 
-export const getGenres = async (req, res) => {
-  try {
-    const url = `https://api.themoviedb.org/3/genre/movie/list`;
-    fetch(url, TMDB_OPTIONS)
-      .then((res) => res.json())
-      .then((json) => {
-        res.json(json);
-      });
-  } catch (error) {
-    res.status(500).json({ message: "something wrong" });
+  const data = await response.json();
+  await saveMoviesToDB(data.results);
+  res.json(data);
+});
+
+export const getGenres = asyncHandler(async (req, res) => {
+  const url = buildTMDBUrl(TMDB_URLS.GENRES);
+
+  const response = await fetch(url, TMDB_OPTIONS);
+  if (!response.ok) {
+    throw new AppError('Failed to fetch genres', response.status);
   }
-};
+
+  const data = await response.json();
+  res.json(data);
+});
